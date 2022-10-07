6497f983526cbf3c6f64b4619afe87df21c82d62
Arjun <im.arjuun@gmail.com>
2022-10-07 21:16:58 +0530

    Initial commit

---
+++ app/src/main/java/com/myapp/ssv2/SignUp.java
@@ -0,0 +1,57 @@
+package com.myapp.ssv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SignUp extends AppCompatActivity {
+
+    TextInputLayout regName, regUsername, regEmail, regPhoneNo, regPassword;
+    Button regBtn, regToLoginBtn;
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        regName = findViewById(R.id.name);
+        regUsername = findViewById(R.id.username);
+        regEmail = findViewById(R.id.email);
+        regPhoneNo = findViewById(R.id.phoneNo);
+        regPassword = findViewById(R.id.password);
+        regBtn = findViewById(R.id.reg_btn);
+        regToLoginBtn = findViewById(R.id.reg_login_btn);
+
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("users");
+
+                UserHelperClass addNewUser = new UserHelperClass(name);
+
+                //Get all values
+                String name = regName.getEditText().getText().toString();
+                String username = regUsername.getEditText().getText().toString();
+                String email = regEmail.getEditText().getText().toString();
+                String phoneNo = regPhoneNo.getEditText().getText().toString();
+                String password = regPassword.getEditText().getText().toString();
+
+
+                 UserHelperClass helperClass = new UserHelperClass();
+
+                reference.child(phoneNo).setValue(helperClass);
+            }
+        });
+    }
+}
